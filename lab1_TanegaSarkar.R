41f788e1d1e813ddf453833a2060e2e6025f0e0d
TanegaSarkar <tanegasarkar@gmail.com>
2025-01-03 18:03:28 +0530

    Add files via upload

---
+++ lab1_TanegaSarkar.R
@@ -0,0 +1,86 @@
+#Lab 1 Jan 3 2025
+
+#Q1.1
+x <- 2.7/2
+print(x)
+#Q1.2
+x <- 2.7%/%2
+print(x)
+y <- 2.7%%2
+print(y)
+#Q1.3
+x <- 10+5i/2
+print(x)
+y <- (10+5i)/2
+print(y)
+#Q1.4
+x <- round(2.5)
+print(x)
+#Q1.5
+x <- round(-2.5)
+print(x)
+#Q1.6
+x <- 2%/%4-1
+print(x)
+#Q1.7
+x <- 3*2**2
+print(x)
+#Q1.8
+x <- 3**2*2
+print(x)
+#Q1.9
+x <- 7%/% 4
+print(x)
+#Q1.10
+x <- 7%4
+print(x)
+#Q1.11
+x <- -7%4
+print(x)
+#Q1.12
+x <- trunc(5.7)
+print(x)
+#Q1.13
+x <- trunc(-5.7)
+print(x)
+
+#Q2
+y <- function(x) floor(x + 0.5)
+print(y(5.7))
+
+#Q3
+a <- 1
+b <- 2
+c <- 4
+x <- a && b
+print (x)
+y <- ! (a<b) || (c>b)
+print(y)
+
+#Q4.1
+x<- c(5,3,7,8)
+print(x)
+#Q4.2
+is.integer(x)
+#Q4.3
+is.numeric(x)
+#Q4.4
+x<- integer(x)
+print(x)
+#integer(x) tries to convert the numeric vector x into an integer type.
+
+
+#Q5.1
+x <- sqrt(2)
+print(x)
+#Q5.2
+y<- x*x ==2
+print(y)
+#Q5.3
+y<- x*x - 2
+print(y)
+#Q5.4
+all.equal(x*x,2)
+  
+
+
